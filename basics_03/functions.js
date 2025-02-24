91c3229ebd6b628c95b44f474afa706543bf6c2a
TSS-sniper <tarunsinghsheoran9703@gmail.com>
2025-02-25 02:55:56 +0530

    file1 b3

--- basics_03/functions.js
+++ basics_03/functions.js
@@ -9,15 +9,58 @@ function func1(){
 // func1 //give reference of function
 // func1()
 
-function add2Num(n1,n2){
-    console.log(n1+n2)
+// function add2Num(n1,n2){
+//     console.log(n1+n2)
+// }
+
+
+function improved_add2Num(n1,n2){
+    let result = n1+n2
+    return result
+    //or return n1+n2
+    console.log("Unreachable Code"); //never gets printed
+    
 }
 
-add2Num(9,5)
-add2Num(9,null)
-add2Num(6,"k") //concatenate
+// add2Num(9,null)
+// add2Num(6,"k") //concatenate
+// add2Num(6,"3") //concatenate
 
-// git branch -m main master
-// git fetch origin
-// git branch -u origin/master master
-// git remote set-head origin -a
+const result = improved_add2Num(9,5)
+//console.log("Result: ",result);
+
+function LogInUserMsg(username){
+    if(!username){
+        console.log("Please enter a username");
+        return 0
+    }
+    return `${username} just logged In`
+}
+
+//console.log(LogInUserMsg("TSS"))
+//console.log(LogInUserMsg()) //gives undefined
+
+
+function CalcCartPrice(val1,...num1){ //here "..." is rest operator
+    return num1
+}
+
+//console.log(CalcCartPrice(100,200,300)); //returns array
+// val1=100 and rest in num1 as array ie [200,300]
+
+const user1 = {
+    username: "Mr.T",
+    bill: 100
+}
+
+function handleUser(anyobject){
+    console.log(`User name is ${anyobject.username} and his bill is of ${anyobject.bill}`);  
+}
+
+//handleUser(user1)
+//alternative call method
+
+handleUser({username: "Yeti",
+    bill:500
+})
+
