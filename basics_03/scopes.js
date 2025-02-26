60da2598284e6296f104a855b57b836dcaaa8beb
TSS-sniper <tarunsinghsheoran9703@gmail.com>
2025-02-26 21:04:11 +0530

    updated scope file

--- basics_03/scopes.js
+++ basics_03/scopes.js
@@ -6,11 +6,52 @@ if(true){ //block scope start
     let a = 20
     const b = 99
     var c = 77
-    console.log("Inner: ",a);
+    //console.log("Inner: ",a);
     
 } //block scope end
 
 //console.log(b); //same
 //console.log(c); //var
-console.log(a);
+//console.log(a);
+
+
+function one(){
+    const username = "ABC"
+
+    function two(){
+        const website  = "YT"
+        console.log(username);
+    }
+    //console.log(website); //gives error
+    //two()
+}
+//two can access variables from one but one cant access variables from two
+//one()
+
+// if (true){
+//     const username = "TSS"
+//     if(username==="TSS"){
+//         const web = " YT"
+//         console.log(username + web);
+//     }
+    //console.log(web); //will give error 
+//}
+//console.log(username); //will give error
+
+//=============================EXPRESSIONS and HOISTING==========================
+
+console.log(addone(9)) //runs ok
+
+function addone(num){
+    return num + 1
+}
+
+//console.log(addtwo(9)); //will give error
+
+const addtwo = function(num){ //expression
+    return num+2
+}
+
+console.log(addtwo(2)); //runs ok
+
 
