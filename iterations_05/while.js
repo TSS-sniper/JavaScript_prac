6d483b836cd02dd14c863ab0665675fc93a4c19d
TSS-sniper <tarunsinghsheoran9703@gmail.com>
2025-03-05 02:40:50 +0530

    ..

---
+++ iterations_05/while.js
@@ -0,0 +1,24 @@
+//WHILE LOOP
+
+let i =0
+while (i<=10) {
+    //console.log(`Value of i is ${i}`);
+    i=i+2
+}
+
+let index=0
+const myarr = ['a','b','t']
+
+while (index<myarr.length) {
+    console.log(`value: ${myarr[index]}`);
+    index++
+}
+
+//do-while loop
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`); // does work first
+    score++
+} while (score<=10) //checks condition later
